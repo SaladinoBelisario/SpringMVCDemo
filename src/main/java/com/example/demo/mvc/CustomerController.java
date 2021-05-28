9be4398fa37a11d1472aa27c3998edc73ef9b5d0
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-05-27 19:12:00 -0500

    Trim whitespaces with InitBinder

--- src/main/java/com/example/demo/mvc/CustomerController.java
+++ src/main/java/com/example/demo/mvc/CustomerController.java
@@ -1,16 +1,42 @@
 package com.example.demo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    //adding initBinder to convert all of our data entries to trimmed data
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model theModel){
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
     }
+
+    @RequestMapping("/processForm")
+    public String processForm(
+        @Valid @ModelAttribute("customer") Customer theCustomer,
+                BindingResult theBindingResult){
+        System.out.println("LastName: |" + theCustomer.getLastName() + "|");
+        if (theBindingResult.hasErrors()){
+            return "customer-form";
+        }else {
+            return "customer-confirmation";
+        }
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/view/customer-confirmation.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Luis
+  Date: 27/05/2021
+  Time: 18:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<head>
+    <title>Customer Confirmation Page</title>
+</head>
+<body>
+    The customer is confirmed: ${customer.firstName} ${customer.lastName}
+</body>
+</html>
