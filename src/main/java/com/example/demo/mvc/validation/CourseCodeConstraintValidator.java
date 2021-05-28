37e0021f70c93115a6bb275d5f973fb66163f50a
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-05-27 21:12:35 -0500

    Adding custom validation
    Custom validation with custom Annotations

---
+++ src/main/java/com/example/demo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,26 @@
+package com.example.demo.mvc.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.Annotation;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode courseCode) {
+        coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        boolean result;
+        if (s != null){
+            result = s.startsWith(coursePrefix);
+        }else{
+            result = true;
+        }
+        return result;
+    }
+}
--- src/main/webapp/WEB-INF/view/customer-confirmation.jsp
+++ src/main/webapp/WEB-INF/view/customer-confirmation.jsp
@@ -17,5 +17,7 @@
     Free passes: ${customer.freePasses}
     <br><br>
     Postal code: ${customer.postalCode}
+    <br><br>
+    Course code: ${customer.courseCode}
 </body>
 </html>
